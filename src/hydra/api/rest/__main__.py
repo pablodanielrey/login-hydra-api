9410168e5e82b00f7f0f206e61cf27e1988908dc
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-05 23:37:23 -0300

    error en el ptvsd

--- src/hydra/api/rest/__main__.py
+++ src/hydra/api/rest/__main__.py
@@ -1,6 +1,6 @@
 
 import ptvsd
-ptvsd.enable_attach("secret", address = ('0.0.0.0', 10004))
+ptvsd.enable_attach(address = ('0.0.0.0', 10004))
 
 from hydra.api.rest.wsgi import app
 app.run(host='0.0.0.0', port=10002, debug=False)
