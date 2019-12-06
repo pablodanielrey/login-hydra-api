00c29d92e45a1fc2d10b7c98f31084b144ca2676
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-05 23:32:09 -0300

    agrego el codigo necesario para debuggear remotamente el entorno usando visual studio code.

--- src/hydra/api/rest/__main__.py
+++ src/hydra/api/rest/__main__.py
@@ -1,4 +1,7 @@
 
+import ptvsd
+ptvsd.enable_attach("secret", address = ('0.0.0.0', 10004))
+
 from hydra.api.rest.wsgi import app
-app.run(host='0.0.0.0',port=10002)
+app.run(host='0.0.0.0', port=10002, debug=False)
 
